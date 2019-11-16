28c87318950b85956b7cb10c04baa0b42dfbb49b
Dave <dew1g18@soton.ac.uk>
2019-11-16 19:39:59 +0000

    its a class now OK

--- Hackathon/GetPlaces.py
+++ Hackathon/GetPlaces.py
@@ -1,28 +1,28 @@
-# import requests
-
 import urllib.request, json 
 
+class GetPlaces:
 
-def getPlaces(inquiryString, startLocation, radius): # pubs or what, latlng of user, radius, 
-    # fix inquiry string
-    # inquiryString = str(inquiryString).replace(" ", "%20")#+ "&inputtype=textquery"
-    inputPart = "type=" + inquiryString+ "&"
-    fieldsPart = "fields=formatted_address"+ "&"
-    startLocation = "location="+ startLocation + "&"
-    radius = "radius="+radius+ "&"# in meters
-    params = "fields=formatted_address&"
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + startLocation+ inputPart + fieldsPart +radius +params+"key=AIzaSyDXKLWHJQdqzVI1agSREbzr4AuoBKyUeuE"    
+    def getPlaces(inquiryString, startLocation, radius): # pubs or what, latlng of user, radius, 
+        # fix inquiry string
+        # inquiryString = str(inquiryString).replace(" ", "%20")#+ "&inputtype=textquery"
+        inputPart = "type=" + inquiryString+ "&"
+        fieldsPart = "fields=formatted_address"+ "&"
+        startLocation = "location="+ startLocation + "&"
+        radius = "radius="+radius+ "&"# in meters
+        params = "fields=formatted_address&"
+        url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + startLocation+ inputPart + fieldsPart +radius +params+"key=AIzaSyDXKLWHJQdqzVI1agSREbzr4AuoBKyUeuE"    
 
-    print (url)
-    # with urllib.request.urlopen(urlToRead) as url:
-    #     data = json.loads(url.read().decode())
-    #     print(data)
-    response = urllib.request.urlopen(url)
+        # print (url)
+        # with urllib.request.urlopen(urlToRead) as url:
+        #     data = json.loads(url.read().decode())
+        #     print(data)
+        response = urllib.request.urlopen(url)
 
-    data = json.loads(response.read())
+        data = json.loads(response.read())
+
+        # print (data['formatted_address'])
+
+        print (data)
 
 
-    print (data)
-
-
-getPlaces("bar", "51.7520220,-1.2577260", "50")
+# getPlaces("bar", "51.7520220,-1.2577260", "50")
