3413dd24b4461509b2c1198577ceafcabc689326
Benjamin Rees <br7g18@soton.ac.uk>
2019-11-16 13:26:01 +0000

    Initial Commit for user input functions

---
+++ Hackathon/UserData.py
@@ -0,0 +1,13 @@
+import googlemaps
+
+#encapsulates 
+class UserData :
+	def __init__(self, location,distance) :
+		self.location = location
+		self.distance = distance
+
+	def getLocation() :
+		return location
+
+	def getDistance() :
+		return distance
